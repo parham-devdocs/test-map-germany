336406a20ca65a1523b2c4e524b7c327f7a2d570
parham <p.77.pazargadi@gmail.com>
2026-02-15 21:06:46 +0330

    projecy set up

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className=' w-screen h-screen bg-amber-900'>
+    <h1 class="text-3xl font-bold underline text-amber-600">
+    Hello world!
+  </h1>   
+    </div>
+
+    </>
+  )
+}
+
+export default App
